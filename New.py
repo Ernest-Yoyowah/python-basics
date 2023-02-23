f15802609c7ce06211eb8952e5cfa6b518b6c7bf
ernkeyz <exconzy26@gmail.com>
2023-02-23 17:14:36 -0500

    first commit

---
+++ New.py
@@ -0,0 +1,11 @@
+names = ["Kelvin", "Bond", "John","Doe", "George","Fort", "Henry","Ads"]
+names.append("Joy")
+print(names)
+second_student = print(names[1])
+Fname = "Ernest"
+Lname = "Fort"
+age = "400"
+phone = "0506923484"
+print("Your full name is",Fname,Lname)
+print(phone)
+print(age)
