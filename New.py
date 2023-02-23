bc9ac3676272e4a11cf873bdc4ac291dfbe84919
ernkeyz <exconzy26@gmail.com>
2023-02-23 18:26:06 -0500

    news

--- New.py
+++ New.py
@@ -1,3 +1,4 @@
+#Creating a list for students
 names = ["Kelvin", "Bond", "John","Doe", "George","Fort", "Henry","Ads"]
 names.append("Joy")
 print(names)
@@ -9,3 +10,5 @@ phone = "0506923484"
 print("Your full name is",Fname,Lname)
 print(phone)
 print(age)
+new_c ="cation"
+print(new_c)
